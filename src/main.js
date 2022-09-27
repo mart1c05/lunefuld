fab92b106d2c8f17ccf5d8abd8f6909bbfa5109c
Martha Scales <mart1c05@stud.kea.dk>
2022-09-27 09:46:55 +0200

    cookie

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
 import "./style.css";
+
+const acceptCookie = document.querySelector("#accepter");
+const denyCookie = document.querySelector("#afvis");
+const cookieboks = document.querySelector("#cookieboks");
+
+
+
+
+acceptCookie.addEventListener("click", fjernCookieBoks); 
+denyCookie.addEventListener("click", fjernCookieBoks); 
+
+function fjernCookieBoks() {
+    cookieboks.classList.add("hidden");
+    sessionStorage.setItem("fjerncookieboks", true);
+
+}
+
+if (sessionStorage.getItem("fjerncookieboks")) {
+    fjernCookieBoks.classList.add("hidden");
+}
